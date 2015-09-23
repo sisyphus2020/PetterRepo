c0a49abe583ca9674cfe4cbb8eab394dd2338977
이필승 <sisyphus2020@naver.com>
2015-09-23 10:48:11 +0900

    StoreReviewLikes 등록/수정/삭제 작업

--- PetterService/Controllers/StoreReviewLikesController.cs
+++ PetterService/Controllers/StoreReviewLikesController.cs
@@ -10,11 +10,18 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
 using PetterService.Models;
+using PetterService.Common;
 
 namespace PetterService.Controllers
 {
     public class StoreReviewLikesController : ApiController
     {
+        // 1. 스토어 리뷰 좋아요 리스트 (X)
+        // 2. 스토어 리뷰 좋아요 상세 (X)
+        // 3. 스토어 리뷰 좋아요 등록 (O)
+        // 4. 스토어 리뷰 좋아요 수정 (O)
+        // 5. 스토어 리뷰 좋아요 삭제 (O)
+
         private PetterServiceContext db = new PetterServiceContext();
 
         // GET: api/StoreReviewLikes
@@ -36,20 +43,38 @@ namespace PetterService.Controllers
             return Ok(storeReviewLike);
         }
 
-        // PUT: api/StoreReviewLikes/5
-        [ResponseType(typeof(void))]
-        public async Task<IHttpActionResult> PutStoreReviewLike(int id, StoreReviewLike storeReviewLike)
+        /// <summary>
+        /// PUT: api/StoreReviewLikes/5
+        /// 스토어 리뷰 좋아요 수정
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="reviewLike"></param>
+        /// <returns></returns>
+        [ResponseType(typeof(PetterResultType<StoreReviewLike>))]
+        public async Task<IHttpActionResult> PutStoreReviewLike(int id, StoreReviewLike reviewLike)
         {
+            PetterResultType<StoreReviewLike> petterResultType = new PetterResultType<StoreReviewLike>();
+            List<StoreReviewLike> storeReviewLikes = new List<StoreReviewLike>();
+
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
-            if (id != storeReviewLike.StoreReviewLikeNo)
+            if (id != reviewLike.StoreReviewLikeNo)
             {
                 return BadRequest();
             }
 
+            StoreReviewLike storeReviewLike = await db.StoreReviewLikes.FindAsync(id);
+            if (storeReviewLike == null)
+            {
+                return NotFound();
+            }
+
+            storeReviewLike.StateFlag = StateFlags.Use;
+            storeReviewLike.DateModified = DateTime.Now;
+            storeReviewLike.DateDeleted = null;
             db.Entry(storeReviewLike).State = EntityState.Modified;
 
             try
@@ -68,38 +93,70 @@ namespace PetterService.Controllers
                 }
             }
 
-            return StatusCode(HttpStatusCode.NoContent);
+            return Ok(petterResultType);
         }
 
-        // POST: api/StoreReviewLikes
-        [ResponseType(typeof(StoreReviewLike))]
+        /// <summary>
+        /// POST: api/StoreReviewLikes
+        /// 스토어 리뷰 좋아요 등록
+        /// </summary>
+        /// <param name="storeReviewLike"></param>
+        /// <returns></returns>
+        [ResponseType(typeof(PetterResultType<StoreReviewLike>))]
         public async Task<IHttpActionResult> PostStoreReviewLike(StoreReviewLike storeReviewLike)
         {
+            PetterResultType<StoreReviewLike> petterResultType = new PetterResultType<StoreReviewLike>();
+            List<StoreReviewLike> storeReviewLikes = new List<StoreReviewLike>();
+
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
+            storeReviewLike.StateFlag = StateFlags.Use;
+            storeReviewLike.DateCreated = DateTime.Now;
+            storeReviewLike.DateModified = DateTime.Now;
+
             db.StoreReviewLikes.Add(storeReviewLike);
             await db.SaveChangesAsync();
 
-            return CreatedAtRoute("DefaultApi", new { id = storeReviewLike.StoreReviewLikeNo }, storeReviewLike);
+            storeReviewLikes.Add(storeReviewLike);
+            petterResultType.IsSuccessful = true;
+            petterResultType.JsonDataSet = storeReviewLikes;
+
+            return Ok(petterResultType);
         }
 
         // DELETE: api/StoreReviewLikes/5
-        [ResponseType(typeof(StoreReviewLike))]
+        /// <summary>
+        /// DELETE: api/StoreReviewLikes/5
+        /// 스토어 리뷰 좋아요 삭제
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [ResponseType(typeof(PetterResultType<StoreReviewLike>))]
         public async Task<IHttpActionResult> DeleteStoreReviewLike(int id)
         {
+            PetterResultType<StoreReviewLike> petterResultType = new PetterResultType<StoreReviewLike>();
+            List<StoreReviewLike> storeReviewLikes = new List<StoreReviewLike>();
             StoreReviewLike storeReviewLike = await db.StoreReviewLikes.FindAsync(id);
+
             if (storeReviewLike == null)
             {
                 return NotFound();
             }
 
-            db.StoreReviewLikes.Remove(storeReviewLike);
+            storeReviewLike.StateFlag = StateFlags.Delete;
+            storeReviewLike.DateDeleted = DateTime.Now;
+            db.Entry(storeReviewLike).State = EntityState.Modified;
+
             await db.SaveChangesAsync();
 
-            return Ok(storeReviewLike);
+            storeReviewLikes.Add(storeReviewLike);
+            petterResultType.IsSuccessful = true;
+            petterResultType.JsonDataSet = storeReviewLikes;
+
+            return Ok(petterResultType);
         }
 
         protected override void Dispose(bool disposing)
